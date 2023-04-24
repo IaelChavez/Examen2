1928c236e94f3565128658fcf9f45dcd56996325
Iael Chavez <jcs72484@udelasalle.edu.mx>
2023-04-24 17:21:03 -0600

    Initial Commit

---
+++ Examen2/Examen2App.swift
@@ -0,0 +1,17 @@
+//
+//  Examen2App.swift
+//  Examen2
+//
+//  Created by ISSC_611_2023 on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Examen2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
