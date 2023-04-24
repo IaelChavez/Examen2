f8fd7d65d79979338fac17f6a7039a2d8bbb9444
Iael Chavez <josueiael@hotmail.com>
2023-04-24 17:48:37 -0600

    prueba5

--- Examen2/ContentView.swift
+++ Examen2/ContentView.swift
@@ -15,8 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
             Text("Hello, world!")
-            Text("Hello, world!")
-            Text("Hello, world!")    
+        
         }
         .padding()
     }
