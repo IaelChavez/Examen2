a9c9210ca65b8a66294bef761a7b8abe9e8ebb2e
Iael Chavez <jcs72484@udelasalle.edu.mx>
2023-04-24 17:24:35 -0600

    Pruebas repo

---
+++ Menu.swift
@@ -0,0 +1,20 @@
+//
+//  Menu.swift
+//  Examen2
+//
+//  Created by ISSC_611_2023 on 24/04/23.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Menu_Previews: PreviewProvider {
+    static var previews: some View {
+        Menu()
+    }
+}
